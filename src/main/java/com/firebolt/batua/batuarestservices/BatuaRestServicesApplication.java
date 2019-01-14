cd2d70c8b75088ffe7c5a4b9d138b0907aa0a0b4
Onkar Mishra <mishraomi@yahoo.in>
2019-01-14 07:12:12 +0530

    - Initial Commit

---
+++ src/main/java/com/firebolt/batua/batuarestservices/BatuaRestServicesApplication.java
@@ -0,0 +1,14 @@
+package com.firebolt.batua.batuarestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatuaRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatuaRestServicesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/firebolt/batua/batuarestservices/BatuaRestServicesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.firebolt.batua.batuarestservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatuaRestServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
